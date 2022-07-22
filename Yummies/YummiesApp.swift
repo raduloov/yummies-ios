79da8f57b0d5bbddee45477e7d743cc4424e6bce
raduloov <raduloov@gmail.com>
2022-07-22 13:23:31 +0300

    Initial Commit

---
+++ Yummies/YummiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiesApp.swift
+//  Yummies
+//
+//  Created by Yavor Radulov on 22.07.22.
+//
+
+import SwiftUI
+
+@main
+struct YummiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
